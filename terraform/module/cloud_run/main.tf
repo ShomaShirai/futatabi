b24079146b8b15257e9840223fbb96c060598ecb
ShomaShirai <takosu9314@gmail.com>
2026-03-20 22:26:48 +0900

    feat: main.tfに環境変数を追加

--- terraform/module/cloud_run/main.tf
+++ terraform/module/cloud_run/main.tf
@@ -28,8 +28,119 @@ resource "google_cloud_run_v2_service" "this" {
 
     containers {
       image = var.image_uri
+
       ports {
         container_port = 8080
+      }
+
+      env {
+        name = "DATABASE_URL"
+        value_source {
+          secret_key_ref {
+            secret  = "backend-database-url"
+            version = "latest"
+          }
+        }
+      }
+
+      env {
+        name = "DEBUG"
+        value_source {
+          secret_key_ref {
+            secret  = "backend-debug"
+            version = "latest"
+          }
+        }
+      }
+
+      env {
+        name = "FIREBASE_PROJECT_ID"
+        value_source {
+          secret_key_ref {
+            secret  = "backend-firebase-project-id"
+            version = "latest"
+          }
+        }
+      }
+
+      env {
+        name = "GCS_BUCKET_NAME"
+        value_source {
+          secret_key_ref {
+            secret  = "backend-gcs-bucket-name"
+            version = "latest"
+          }
+        }
+      }
+
+      env {
+        name = "GOOGLE_PLACES_API_KEY"
+        value_source {
+          secret_key_ref {
+            secret  = "backend-google-places-api-key"
+            version = "latest"
+          }
+        }
+      }
+
+      env {
+        name = "GOOGLE_PLACES_ENDPOINT"
+        value_source {
+          secret_key_ref {
+            secret  = "backend-google-places-endpoint"
+            version = "latest"
+          }
+        }
+      }
+
+      env {
+        name = "GOOGLE_PLACES_LANGUAGE_CODE"
+        value_source {
+          secret_key_ref {
+            secret  = "backend-google-places-language-code"
+            version = "latest"
+          }
+        }
+      }
+
+      env {
+        name = "GOOGLE_PLACES_REGION_CODE"
+        value_source {
+          secret_key_ref {
+            secret  = "backend-google-places-region-code"
+            version = "latest"
+          }
+        }
+      }
+
+      env {
+        name = "GEMINI_API_KEY"
+        value_source {
+          secret_key_ref {
+            secret  = "backend-gemini-api-key"
+            version = "latest"
+          }
+        }
+      }
+
+      env {
+        name = "GEMINI_API_BASE_URL"
+        value_source {
+          secret_key_ref {
+            secret  = "backend-gemini-api-base-url"
+            version = "latest"
+          }
+        }
+      }
+
+      env {
+        name = "GEMINI_MODEL"
+        value_source {
+          secret_key_ref {
+            secret  = "backend-gemini-model"
+            version = "latest"
+          }
+        }
       }
     }
   }
